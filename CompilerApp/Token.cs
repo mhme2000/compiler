62300aa59b4e2691f3bd03161da8bebb2967b052
Marcos Eckart <mhme2000@hotmail.com>
2022-07-18 22:33:07 -0400

    Semantic v0

--- CompilerApp/Token.cs
+++ CompilerApp/Token.cs
@@ -2,15 +2,6 @@
 
 public class Token
 {
-    public TypeToken Type { get; init; }
+    public EnumTypeToken Type { get; init; }
     public string Content { get; init; } = null!;
-
-    public enum TypeToken
-    {
-        Identifier = 1,
-        Operator = 2,
-        Bundler = 3,
-        LineBreak = 4,
-        EndOfChain = 5,
-    }
 }
